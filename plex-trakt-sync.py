331bce29effc6f3a63e994d0babfbfee70a42dc6
Daniel Maxwell <daniel.maxwell@decoderstudios.com>
2013-06-21 14:38:58 -0400

    Syntax error

--- plex-trakt-sync.py
+++ plex-trakt-sync.py
@@ -378,7 +378,7 @@ class Syncer(object):
 	def trakt_report_movies(self, nodes):
 		password = ''
 
-		if self.options.trakt_password_hash
+		if self.options.trakt_password_hash:
 			password = self.options.trakt_password_hash
 		else:
 			password = self.options.trakt_password
